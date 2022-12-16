18c1a8af2871fc02db239d2c7b358bdffdb68231
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2022-12-16 15:01:45 +0400

    add FAQ, brand, footer

--- js/main.js
+++ js/main.js
@@ -9,9 +9,10 @@ owl.owlCarousel({
     center: true,
     loop: true,/*Чтоб зациклить */
     // margin: 20,
-    // autoplay: true,
-    // autoplayTimeout: 6500,
-    // autoplayHoverPause:false,
+    autoplay: true,
+    autoplayTimeout: 6500,
+    autoplayHoverPause: false,
+    smartSpeed: 800,
     // animateOut: 'fadeOut',
     startPosition: 1, /*какой элемент будет активный*/
     items: 1,//Количество слайдов
@@ -24,4 +25,21 @@ owl.owlCarousel({
             items: 1,
         },
 }})
+
+$(document).ready(function(){
+    $(".set > a").on("click", function(){
+      if($(this).hasClass('active')){
+        $(this).removeClass("active");
+        $(this).siblings('.content').slideUp(200);
+        $(".set > a span").removeClass("minus").addClass("plus");
+      }else{
+        $(".set > a span").removeClass("minus").addClass("plus");
+        $(this).find("span").removeClass("plus").addClass("minus");
+        $(".set > a").removeClass("active");
+        $(this).addClass("active");
+        $('.content').slideUp(200);
+        $(this).siblings('.content').slideDown(200);
+      }    
+    });
+  });
 
