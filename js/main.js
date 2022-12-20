ca804194198a6b67404e61589ab253224699354e
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2022-12-20 13:36:28 +0400

    add popup

--- js/main.js
+++ js/main.js
@@ -26,11 +26,26 @@ owl.owlCarousel({
         },
 }})
 
+
+
+$('.new-slider').owlCarousel({
+  nav: true,
+  items:1,
+  loop: false,
+  center:true,
+  autoplay: false,
+  margin:10,
+  URLhashListener:true,
+  autoplayHoverPause:true,
+  startPosition: 'URLHash'
+});
+
+
 $(document).ready(function(){
     $(".set > a").on("click", function(){
       if($(this).hasClass('active')){
         $(this).removeClass("active");
-        $(this).siblings('.content').slideUp(200);
+        $(this).siblings('.content').slideUp(300);
         $(".set > a span").removeClass("minus").addClass("plus");
       }else{
         $(".set > a span").removeClass("minus").addClass("plus");
@@ -38,8 +53,44 @@ $(document).ready(function(){
         $(".set > a").removeClass("active");
         $(this).addClass("active");
         $('.content').slideUp(200);
-        $(this).siblings('.content').slideDown(200);
+        $(this).siblings('.content').slideDown(300);
       }    
     });
   });
+
+
+  //--------------------------------------
+
+let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+  button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+      popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+      popup.classList.add('active'); // И для самого окна
+  })
+})
+
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+  popupBg.classList.remove('active'); // Убираем активный класс с фона
+  popup.classList.remove('active'); // И с окна
+  console.log('закрыли')
+});
+
+
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+  if(e.target === popupBg) { // Если цель клика - фот, то:
+      popupBg.classList.remove('active'); // Убираем активный класс с фона
+      popup.classList.remove('active'); // И с окна
+  }
+  console.log('закрыли 2')
+});
+
+
+
+
+
 
